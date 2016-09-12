02cf63948367f94b62dc744f501d538eac224807
s-mckenzie <1493236@p322-31.studentservers.jac-servers.johnabbott.qc.ca>
2016-09-12 12:45:39 -0400

    foo
    bar

--- testapp/app/src/main/java/com/example/a1493236/testapp/MainActivityFragment.java
+++ testapp/app/src/main/java/com/example/a1493236/testapp/MainActivityFragment.java
@@ -17,7 +17,6 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
-        return root;
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
